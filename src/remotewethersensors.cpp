b801693daf2bdad774a6677c2a15fa03a9692dba
Gleb K <49366600+Glebonsky@users.noreply.github.com>
2020-12-09 01:09:41 +0300

    Added remote weather station class

--- src/remotewethersensors.cpp
+++ src/remotewethersensors.cpp
@@ -1,18 +1,47 @@
 #include "remotewethersensors.h"
 
 
-RemoteTemperature::RemoteTemperature()
+RemoteTemperature::RemoteTemperature(RemoteWeatherSensors *parent)
+    : _parent(parent)
 {}
 
 double RemoteTemperature::value()
 {
-    return -1;
+    return _parent->_lastTemp;
 }
 
-RemoteHumidit::RemoteHumidit()
+RemoteHumidit::RemoteHumidit(RemoteWeatherSensors *parent)
+    : _parent(parent)
 {}
 
 double RemoteHumidit::value()
 {
-    return -1;
+    return _parent->_lastHumid;
+}
+
+RemoteWeatherSensors::RemoteWeatherSensors(unsigned short port)
+    : _temp(this),
+      _humid(this),
+      _connection( port, [this] (char *data, int size) {this->messageReceived(data,size);} )
+{}
+
+Sensor *RemoteWeatherSensors::tempSensor()
+{
+    return &_temp;
+}
+
+Sensor *RemoteWeatherSensors::humidSensor()
+{
+    return &_humid;
+}
+
+void RemoteWeatherSensors::messageReceived(char *data, int size)
+{
+    if (size != 4) return;
+
+    short *temp = reinterpret_cast<short *>(data);
+    short *humid = reinterpret_cast<short *>(data + 2);
+
+    _lastTemp = (*temp)/10.;
+    _lastHumid = (*humid)/10.;
 }
--- src/remotewethersensors.h
+++ src/remotewethersensors.h
@@ -4,12 +4,17 @@
 #include "isensor.h"
 
 
+class RemoteWeatherSensors;
+
 class RemoteTemperature : public Sensor
 {
 public:
-    RemoteTemperature();
+    RemoteTemperature(RemoteWeatherSensors *parent);
 
     double value() override;
+
+private:
+    RemoteWeatherSensors *const _parent;
 };
 
 
@@ -17,9 +22,37 @@ public:
 class RemoteHumidit : public Sensor
 {
 public:
-    RemoteHumidit();
+    RemoteHumidit(RemoteWeatherSensors *parent);
 
     double value() override;
+
+private:
+    RemoteWeatherSensors *const _parent;
+};
+
+
+#include "tcpconnection.h"
+
+class RemoteWeatherSensors
+{
+public:
+    RemoteWeatherSensors(unsigned short port);
+
+    Sensor *tempSensor();
+    Sensor *humidSensor();
+
+private:
+    double _lastTemp = -1;
+    double _lastHumid = -1;
+
+    friend RemoteTemperature;
+    friend RemoteHumidit;
+
+    RemoteTemperature _temp;
+    RemoteHumidit _humid;
+
+    TcpConnection _connection;
+    void messageReceived(char *data, int size);
 };
 
 #endif // REMOTEWETHERSENSORS_H
