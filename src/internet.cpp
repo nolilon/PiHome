51095dcfa72279e4b8b97330a1f2ff4553040d53
Gleb K <49366600+Glebonsky@users.noreply.github.com>
2020-12-10 14:19:52 +0300

    Remake for callback and qt event loop

--- src/internet.cpp
+++ src/internet.cpp
@@ -1,39 +1,39 @@
 #include "internet.h"
 
-#include "bottoken.h"
-#include <QCoreApplication>
 
 InternetPc::InternetPc()
 {}
 
-void InternetPc::loop()
-{
-    QCoreApplication::processEvents();
-    if ( _replyObj != nullptr && _replyObj->isFinished() )
-    {
-        QTextStream str(_replyObj);
-        _replyStr = str.readAll().toStdString().c_str();
-        _replyReady = true;
-
-//        if (_replyStr.length() > 0)
-//            qDebug() << _replyStr;
-    }
-}
-
 void InternetPc::get(QString url)
 {
     _replyStr.clear();
-    if ( _replyObj != nullptr ) _replyObj->abort();
+    if ( _replyObj != nullptr )
+    {
+        delete _replyObj;
+        _replyObj = nullptr;
+    }
 
     auto encodedUrl = QUrl( url );
     _replyObj = _internet.get(QNetworkRequest(encodedUrl));
-    _replyReady = false;
+    QObject::connect( _replyObj, &QNetworkReply::finished, [this] () {this->requestDone();} );
 //    qDebug() << "Get request sent:\n" << url;
 }
 
-bool InternetPc::replyReady() const
+void InternetPc::requestDone()
 {
-    return _replyReady;
+    _replyStr = _replyObj->readAll();
+    _replyObj->deleteLater();
+    _replyObj = nullptr;
+
+    _onReply();
+
+//    if (_replyStr.length() > 0)
+//        qDebug() << _replyStr;
+}
+
+void InternetPc::setOnReply(Callback callback)
+{
+    _onReply = callback;
 }
 
 QString InternetPc::reply() const
--- src/internet.h
+++ src/internet.h
@@ -2,14 +2,16 @@
 #define INTERNET_H
 
 #include <QString>
+#include <functional>
+
+typedef std::function<void()> Callback;
 
 class Internet
 {
 public:
-    virtual void loop() = 0;
-
     virtual void get(QString url) = 0;
-    virtual bool replyReady() const = 0;
+
+    virtual void setOnReply(Callback callback) = 0;
     virtual QString reply() const = 0;
 
     virtual ~Internet() {}
@@ -24,18 +26,17 @@ class InternetPc : public Internet
 public:
     InternetPc();
 
-    void loop() override;
-
     void get(QString url) override;
-    bool replyReady() const override;
+    void setOnReply(Callback callback) override;
     QString reply() const override;
 
 private:
+    void requestDone();
     QString _replyStr = "";
+    Callback _onReply = nullptr;
 
     QNetworkAccessManager _internet;
     QNetworkReply *_replyObj = nullptr;
-    bool _replyReady = false;
 };
 
 #endif // INTERNET_H
