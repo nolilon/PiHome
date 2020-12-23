052ed03ec75174a06e3277833c4a409f475a991a
Gleb K <49366600+Glebonsky@users.noreply.github.com>
2020-12-24 01:30:57 +0300

    Doesn't need loop any more

--- src/device.h
+++ src/device.h
@@ -7,8 +7,9 @@ class Device
 public:
     virtual void turnOn() = 0;
     virtual void turnOff() = 0;
+    virtual void toggle() = 0;
 
-    virtual void loop() = 0;
+    virtual bool isOn() = 0;
 
     virtual ~Device() {}
 };
