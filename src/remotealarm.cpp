db97052712bf58d94de2c1af667ab07b8d09dc29
Gleb K <49366600+Glebonsky@users.noreply.github.com>
2020-12-24 22:04:17 +0300

    Added interface implementation

--- src/remotealarm.cpp
+++ src/remotealarm.cpp
@@ -1,12 +1,41 @@
 #include "remotealarm.h"
 
+#include <QSettings>
+
+
+
 RemoteAlarm::RemoteAlarm(unsigned short port)
-    : _connection(port, [this] (char *data, int size) {this->messageReceived(data,size);} ),
-      _startTime(8,45)
+    : _connection(port, [this] (char *data, int size) {this->messageReceived(data,size);} )
 {
+    QSettings config(_alarmConfig, QSettings::IniFormat);
+    _startTime = config.value("startTime").toTime();
+
     _timer.setInterval(5000);
     QObject::connect( &_timer, &QTimer::timeout, [this] () {this->timeCheck();} );
     _timer.start();
+}
+
+void RemoteAlarm::setTime(Time newTime)
+{
+    _startTime = QTime(newTime.hours, newTime.minutes);
+    QSettings config(_alarmConfig, QSettings::IniFormat);
+    config.setValue("startTime", _startTime);
+}
+
+Time RemoteAlarm::time() const
+{
+    return {_startTime.hour(), _startTime.minute()};
+}
+
+bool RemoteAlarm::isConnected()
+{
+    return true;
+}
+
+void RemoteAlarm::stop()
+{
+    char command = 'F';
+    _connection.send(&command, 1);
 }
 
 void RemoteAlarm::timeCheck()
--- src/remotealarm.h
+++ src/remotealarm.h
@@ -1,20 +1,28 @@
 #ifndef REMOTEALARM_H
 #define REMOTEALARM_H
 
+#include "ialarm.h"
 #include "tcpconnection.h"
 #include <QTimer>
 #include <QTime>
 
-class RemoteAlarm
+class RemoteAlarm : public Alarm
 {
 public:
     RemoteAlarm(unsigned short port);
+
+    void setTime(Time newTime) override;
+    Time time() const override;
+    bool isConnected() override;
+    void stop() override;
 
 private:
     TcpConnection _connection;
     QTimer _timer;
     QTime _startTime;
     bool _done = false;
+
+    QString const _alarmConfig = "alarm.ini";
 
     void timeCheck();
     void messageReceived(char *data, int size);
