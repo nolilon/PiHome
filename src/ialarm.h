5a68bc20fc3a9f022476e027396dcf84695d0d4f
Gleb K <49366600+Glebonsky@users.noreply.github.com>
2020-12-24 22:03:32 +0300

    Alarm interface added

---
+++ src/ialarm.h
@@ -0,0 +1,21 @@
+#ifndef IALARM_H
+#define IALARM_H
+
+struct Time
+{
+    int hours = 0;
+    int minutes = 0;
+};
+
+class Alarm
+{
+public:
+    virtual void setTime(Time newTime) = 0;
+    virtual Time time() const = 0;
+    virtual bool isConnected() = 0;
+    virtual void stop() = 0;
+
+    ~Alarm() {}
+};
+
+#endif // IALARM_H
