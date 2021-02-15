a35fc8c85bbd8caedc0132014156e462929dce5b
Gleb K <49366600+Glebonsky@users.noreply.github.com>
2021-02-15 22:08:46 +0300

    Logging to file added

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,23 @@
 #include "mainlogic.h"
 #include "view.h"
 #include "ports.h"
-#include <QCoreApplication>
 
 #include "remotewethersensors.h"
 #include "remotealarm.h"
 #include "remotelight.h"
 
+#include <QCoreApplication>
+#include <QFile>
+const QString logFileName = "log.txt";
+
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg);
+
 
 int main(int argc, char **argv)
 {
     QCoreApplication a(argc, argv);
+    QFile::remove(logFileName);
+    qInstallMessageHandler(myMessageOutput);
 
     InternetPc internet;
 
@@ -31,3 +38,41 @@ int main(int argc, char **argv)
     a.exec();
 }
 
+
+#include <QDateTime>
+#include <QMutex>
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    Q_UNUSED(context);
+    QByteArray localMsg = msg.toLocal8Bit();
+
+    static QMutex mutex;
+    QFile outFile(logFileName);
+
+    mutex.lock();
+    outFile.open(QIODevice::Append);
+    QTextStream ts(&outFile);
+    ts << QDateTime::currentDateTime().toString("dd.MM.yy hh:mm:ss");
+
+    switch (type) {
+    case QtDebugMsg:
+        ts << " D: " + msg + '\n';
+        break;
+    case QtInfoMsg:
+        ts << " I: " + msg + '\n';
+        break;
+    case QtWarningMsg:
+        ts << " W: " + msg + '\n';
+        break;
+    case QtCriticalMsg:
+        ts << " C: " + msg + '\n';
+        break;
+    case QtFatalMsg:
+        ts << " Fatal: " + msg + '\n';
+        mutex.unlock();
+        abort();
+        break;
+    }
+    outFile.close();
+    mutex.unlock();
+}
